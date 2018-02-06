29faaa66df9553aed4b7ddef32ada50915fa4f9b
Sandeep Singh <wwaheguru9509088985@gmail.com>
2018-02-06 20:31:23 +0530

    commit

--- microservices/api/src/server.js
+++ microservices/api/src/server.js
@@ -22,35 +22,41 @@ app.use(bodyParser.urlencoded({
 app.use('/', hasuraRouter);
 
 app.post("/delete",function(req,res){
-  var hasura_id = req.body.hasura_id;
-  console.log(hasura_id)
-  console.log(hasura_id+" yeah i am here")
-  var deleteOptions = {
-    url: config.projectConfig.url.auth.delete_user,
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'X-Hasura-User-Id': 1,
-      'X-Hasura-Role': 'admin',
-      "X-Hasura-Allowed-Roles": "user,admin"
-    },
-    body: JSON.stringify({
-        "hasura_id": hasura_id
-    })
-  }
-  request(deleteOptions, function(error, response, body) {
-    console.log("response 1:-"+response)
-    if (error) {
-        console.log('Error from delete-user request: ');
-        console.log(error)
-        res.status(500).json({
-          'error': error,
-          'message': 'delete-user request failed'
-        });
+  //var hasura_id = req.body.hasura_id;
+  console.log(req.headers);
+  const hasura_id = req.headers['X-Hasura-User-Id'];
+  if( hasura_id > 1){
+      var deleteOptions = {
+        url: config.projectConfig.url.auth.delete_user,
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Hasura-User-Id': 1,
+          'X-Hasura-Role': 'admin',
+          "X-Hasura-Allowed-Roles": "user,admin"
+        },
+        body: JSON.stringify({
+            "hasura_id": hasura_id
+        })
+      }
+      request(deleteOptions, function(error, response, body) {
+        console.log("response 1:-"+response)
+        if (error) {
+            console.log('Error from delete-user request: ');
+            console.log(error)
+            res.status(500).json({
+              'error': error,
+              'message': 'delete-user request failed'
+            });
+        }
+        res.json(JSON.parse(body))
+      })
     }
-    res.json(JSON.parse(body))
-  })
-
+    else{
+      res.status(500).json({
+        'message': 'acees denied! that it. delete-user request failed'
+      });
+    }
 });
 
 app.listen(8080, function () {
