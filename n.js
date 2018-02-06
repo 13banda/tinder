ff83bbac8a83526918a80a6eee3e91d3a076271d
Sandeep Singh <wwaheguru9509088985@gmail.com>
2018-02-06 19:33:16 +0530

    comit

--- n.js
+++ n.js
@@ -1,2 +1,25 @@
-let v = 5;
-console.log(v);
+var url = "https://api.gamey34.hasura-app.io/delete";
+
+var requestOptions = {
+    "method": "POST",
+    "headers": {
+        "Content-Type": "application/json"
+    }
+};
+
+var body = {
+    "hasura_id": 5,
+};
+
+requestOptions.body = JSON.stringify(body);
+
+fetch(url, requestOptions)
+.then(function(response) {
+	return response.json();
+})
+.then(function(result) {
+	console.log(result);
+})
+.catch(function(error) {
+	console.log('Request Failed:' + error);
+});
