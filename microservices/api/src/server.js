ffd5548280e36a03506cb1b032421d8b34f2e0d3
Sandeep Singh <wwaheguru9509088985@gmail.com>
2018-02-07 20:49:26 +0530

    commit api-end point

--- microservices/api/src/server.js
+++ microservices/api/src/server.js
@@ -20,6 +20,267 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use('/', hasuraRouter);
+// like other user
+app.post("/like",function(req,res){
+  const hasura_id = req.headers['x-hasura-user-id'];
+  //we can't allow to delete admin from this api
+  if( hasura_id > 1){
+    const hasura_role = req.headers['x-hasura-role'];
+    const hasura_allowed_role = req.headers['x-hasura-allowed-roles'];
+    const like_user_id = req.body.like_user_id;
+      //Fetch all rows from table - articles
+      var selectOptions = {
+        url: config.projectConfig.url.data,
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Hasura-User-Id': hasura_id,
+          'X-Hasura-Role': hasura_role,
+          "X-Hasura-Allowed-Roles": hasura_allowed_role
+        },
+        body: JSON.stringify({
+          "type": "insert",
+          "args": {
+              "table": "match",
+              "objects": [
+                  {
+                      "like_user_id": like_user_id,
+                      "hasura_id": hasura_id
+                  }
+              ]
+          }
+        })
+      }
+      request(selectOptions, function(error, response, body) {
+        if (error) {
+            console.log(error)
+            res.status(500).json({
+              'error': error,
+              'message': 'like request failed'
+            });
+        }
+        res.json(JSON.parse(body))
+      })
+    }
+    else{
+      res.status(500).json({
+        'message': 'like request failed'
+      });
+    }
+});
+//nope other user with his hasura_id
+app.post("/nope",function(req,res){
+  const hasura_id = req.headers['x-hasura-user-id'];
+  //we can't allow to delete admin from this api
+  if( hasura_id > 1){
+    const hasura_role = req.headers['x-hasura-role'];
+    const hasura_allowed_role = req.headers['x-hasura-allowed-roles'];
+    const like_user_id = req.body.like_user_id;
+      //Fetch all rows from table - articles
+      var selectOptions = {
+        url: config.projectConfig.url.data,
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Hasura-User-Id': hasura_id,
+          'X-Hasura-Role': hasura_role,
+          "X-Hasura-Allowed-Roles": hasura_allowed_role
+        },
+        body: JSON.stringify({
+          "type": "delete",
+          "args": {
+              "table": "match",
+              "where": {
+                  "$and": [
+                      {
+                          "hasura_id": {
+                              "$eq": hasura_id
+                          }
+                      },
+                      {
+                          "like_user_id": {
+                              "$eq": like_user_id
+                          }
+                      }
+                  ]
+              }
+          }
+        })
+      }
+      request(selectOptions, function(error, response, body) {
+        if (error) {
+            console.log(error)
+            res.status(500).json({
+              'error': error,
+              'message': 'nope request failed'
+            });
+        }
+        res.json(JSON.parse(body))
+      })
+    }
+    else{
+      res.status(500).json({
+        'message': 'nope request failed'
+      });
+    }
+});
+// to get list of other like user
+app.get("/like-users",function(req,res){
+  const hasura_id = req.headers['x-hasura-user-id'];
+  //we can't allow to delete admin from this api
+  if( hasura_id > 1){
+    // query to exe get all like user info
+    var query = "select match.like_user_id, userinfo.name,userinfo.profile_file_id from match,userinfo where match.like_user_id = userinfo.hasura_id AND match.hasura_id ="+hasura_id;
+      //Fetch all rows from table - articles
+      var selectOptions = {
+        url: config.projectConfig.url.data,
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Hasura-User-Id': 1,
+          'X-Hasura-Role': "admin",
+          "X-Hasura-Allowed-Roles": "user,admin"
+        },
+        body: JSON.stringify({
+                "type": "run_sql",
+                "args": {
+                  "sql": query
+                }
+              })
+      }
+      request(selectOptions, function(error, response, body) {
+        if (error) {
+            console.log(error)
+            res.status(500).json({
+              'error': error,
+              'message': 'like-users request failed'
+            });
+        }
+        res.json(JSON.parse(body))
+      })
+    }
+    else{
+      res.status(500).json({
+        'message': 'like-users request failed'
+      });
+    }
+});
+
+app.post("/update-user",function(req,res){
+  const hasura_id = req.headers['x-hasura-user-id'];
+  //we can't allow to delete admin from this api
+  if( hasura_id > 1){
+    const hasura_role = req.headers['x-hasura-role'];
+    const hasura_allowed_role = req.headers['x-hasura-allowed-roles'];
+    const name = req.body.name;
+    const email = req.body.email;
+    const age = req.body.age;
+    const about = req.body.about;
+      //Fetch all rows from table - articles
+      var selectOptions = {
+        url: config.projectConfig.url.data,
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Hasura-User-Id': hasura_id,
+          'X-Hasura-Role': hasura_role,
+          "X-Hasura-Allowed-Roles": hasura_allowed_role
+        },
+        body: JSON.stringify({
+          "type": "update",
+          "args": {
+              "table": "userinfo",
+              "where": {
+                  "hasura_id": {
+                      "$eq": hasura_id
+                  }
+              },
+              "$set": {
+                  "email": email,
+                  "name": name,
+                  "age": age,
+                  "about_me": about
+              }
+          }
+        })
+      }
+      request(selectOptions, function(error, response, body) {
+        if (error) {
+            console.log(error)
+            res.status(500).json({
+              'error': error,
+              'message': 'update-user request failed'
+            });
+        }
+        res.json(JSON.parse(body))
+      })
+    }
+    else{
+      res.status(500).json({
+        'message': 'update-user request failed'
+      });
+    }
+});
+
+app.post("/insert-user",function(req,res){
+  const hasura_id = req.headers['x-hasura-user-id'];
+  //we can't allow to delete admin from this api
+  if( hasura_id > 1){
+    const hasura_role = req.headers['x-hasura-role'];
+    const hasura_allowed_role = req.headers['x-hasura-allowed-roles'];
+    const name = req.body.name;
+    const email = req.body.email;
+    const gender = req.body.gender;
+    const file_id = req.body.file_id;
+    const age = req.body.age;
+    const about = req.body.about;
+    const city = req.body.city;
+      //Fetch all rows from table - articles
+      var selectOptions = {
+        url: config.projectConfig.url.data,
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Hasura-User-Id': hasura_id,
+          'X-Hasura-Role': hasura_role,
+          "X-Hasura-Allowed-Roles": hasura_allowed_role
+        },
+        body: JSON.stringify({
+          "type": "insert",
+          "args": {
+              "table": "userinfo",
+              "objects": [
+                  {
+                      "hasura_id": hasura_id,
+                      "name": name,
+                      "email": email,
+                      "gender": gender,
+                      "profile_file_id": file_id,
+                      "age":age,
+                      "about_me": about,
+                      "city": city
+                  }
+              ]
+          }
+        })
+      }
+      request(selectOptions, function(error, response, body) {
+        if (error) {
+            console.log(error)
+            res.status(500).json({
+              'error': error,
+              'message': 'insert-user request failed'
+            });
+        }
+        res.json(JSON.parse(body))
+      })
+    }
+    else{
+      res.status(500).json({
+        'message': 'insert-user request failed'
+      });
+    }
+});
 
 app.get("/get-allusers-info",function(req,res){
   const hasura_id = req.headers['x-hasura-user-id'];
